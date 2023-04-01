e58f253770c01511c048d57451bdc7dd18d4ee74
zLuuuck <xx.lyck.nitgh.xx@gmail.com>
2023-03-31 23:52:18 -0300

    Habilidades adicionado. Menu sai após selecionar um tópico

--- static/script.js
+++ static/script.js
@@ -11,3 +11,18 @@ menuMobile.addEventListener('click', () => {
 });
 
 /* Abre e fecha o menu lateral  */
+/* Fecha menu lateral  */
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active");
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
+
+
+/* Fecha menu lateral  */
